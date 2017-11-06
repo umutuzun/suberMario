e71524ca7fade661b325248a423a118fcc473a0b
Umut Uzun <umutu@evam.com>
2017-11-06 20:38:15 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/suberMario.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/suberMario.dir/suberMario.cpp.obj"
+  "suberMario.pdb"
+  "suberMario.exe"
+  "suberMario.exe.manifest"
+  "libsuberMario.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/suberMario.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
