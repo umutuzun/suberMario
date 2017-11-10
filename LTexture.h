a01f922427405cb89f7eb300e695fa900a8b9811
Umut Uzun <umutu@evam.com>
2017-11-10 16:32:30 +0300

    add texture wrapper class

---
+++ LTexture.h
@@ -0,0 +1,43 @@
+//
+// Created by Umut Uzun on 11/10/2017.
+//
+
+#ifndef SUBERMARIO_LTEXTURE_H
+#define SUBERMARIO_LTEXTURE_H
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+
+//Texture wrapper class
+class LTexture  {
+public:
+    //Initializes variables
+    LTexture();
+
+    //Deallocates memory
+    ~LTexture();
+
+    //Loads image at specified path
+    bool loadFromFile( std::string path, SDL_Renderer* gRenderer );
+
+    //Deallocates texture
+    void free();
+
+    //Renders texture at given point
+    void render( int x, int y, SDL_Renderer* gRenderer, SDL_Rect* clip=NULL );
+
+    //Gets image dimensions
+    int getWidth();
+    int getHeight();
+
+private:
+    //The actual hardware texture
+    SDL_Texture* mTexture;
+
+    //Image dimensions
+    int mWidth;
+    int mHeight;
+};
+
+#endif //SUBERMARIO_LTEXTURE_H
--- suberMario.cpp
+++ suberMario.cpp
@@ -3,63 +3,20 @@
 #include <SDL2/SDL_image.h>
 #include <stdio.h>
 #include <string>
+#include "LTexture.h"
 
-//Key press surfaces constants
-enum KeyPressSurfaces
-{
-    KEY_PRESS_SURFACE_DEFAULT,
-    KEY_PRESS_SURFACE_UP,
-    KEY_PRESS_SURFACE_DOWN,
-    KEY_PRESS_SURFACE_LEFT,
-    KEY_PRESS_SURFACE_RIGHT,
-    KEY_PRESS_SURFACE_TOTAL
-};
 
 //Screen dimension constants
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+
 int CLIP_X = 0;
-
-//Texture wrapper class
-class LTexture
-{
-public:
-    //Initializes variables
-    LTexture();
-
-    //Deallocates memory
-    ~LTexture();
-
-    //Loads image at specified path
-    bool loadFromFile( std::string path );
-
-    //Deallocates texture
-    void free();
-
-    //Renders texture at given point
-    void render( int x, int y, SDL_Rect* clip = NULL );
-
-    //Gets image dimensions
-    int getWidth();
-    int getHeight();
-
-private:
-    //The actual hardware texture
-    SDL_Texture* mTexture;
-
-    //Image dimensions
-    int mWidth;
-    int mHeight;
-};
 
 //Starts up SDL and creates window
 bool init();
 
-//Loads media
-bool loadMedia();
-
-//Frees media and shuts down SDL
-void close();
+//The window renderer
+SDL_Renderer* gRenderer = NULL;
 
 //Loads individual image as texture
 SDL_Texture* loadTexture( std::string path );
@@ -67,105 +24,9 @@ SDL_Texture* loadTexture( std::string path );
 //The window we'll be rendering to
 SDL_Window* gWindow = NULL;
 
-//The window renderer
-SDL_Renderer* gRenderer = NULL;
-
 //Scene sprite
 SDL_Rect gSpriteClip;
 LTexture gSpriteSheetTexture;
-
-LTexture::LTexture()
-{
-    //Initialize
-    mTexture = NULL;
-    mWidth = 0;
-    mHeight = 0;
-}
-
-LTexture::~LTexture()
-{
-    //Deallocate
-    free();
-}
-
-bool LTexture::loadFromFile( std::string path )
-{
-    //Get rid of preexisting texture
-    free();
-
-    //The final texture
-    SDL_Texture* newTexture = NULL;
-
-    //Load image at specified path
-    SDL_Surface* loadedSurface = IMG_Load( path.c_str() );
-    if( loadedSurface == NULL )
-    {
-        printf( "Unable to load image %s! SDL_image Error: %s\n", path.c_str(), IMG_GetError() );
-    }
-    else
-    {
-        //Color key image
-        SDL_SetColorKey( loadedSurface, SDL_TRUE, SDL_MapRGB( loadedSurface->format, 0, 0xFF, 0xFF ) );
-
-        //Create texture from surface pixels
-        newTexture = SDL_CreateTextureFromSurface( gRenderer, loadedSurface );
-        if( newTexture == NULL )
-        {
-            printf( "Unable to create texture from %s! SDL Error: %s\n", path.c_str(), SDL_GetError() );
-        }
-        else
-        {
-            //Get image dimensions
-            mWidth = loadedSurface->w;
-            mHeight = loadedSurface->h;
-        }
-
-        //Get rid of old loaded surface
-        SDL_FreeSurface( loadedSurface );
-    }
-
-    //Return success
-    mTexture = newTexture;
-    return mTexture != NULL;
-}
-
-void LTexture::free()
-{
-    //Free texture if it exists
-    if( mTexture != NULL )
-    {
-        SDL_DestroyTexture( mTexture );
-        mTexture = NULL;
-        mWidth = 0;
-        mHeight = 0;
-    }
-}
-
-void LTexture::render( int x, int y, SDL_Rect* clip )
-{
-    //Set rendering space and render to screen
-    SDL_Rect renderQuad = { x, y, mWidth, mHeight };
-
-    //Set clip rendering dimensions
-    if( clip != NULL )
-    {
-        renderQuad.w = clip->w;
-        renderQuad.h = clip->h;
-    }
-
-    //Render to screen
-    SDL_RenderCopy( gRenderer, mTexture, clip, &renderQuad );
-}
-
-int LTexture::getWidth()
-{
-    return mWidth;
-}
-
-int LTexture::getHeight()
-{
-    return mHeight;
-}
 
 bool init()
 {
@@ -230,13 +91,13 @@ void setSpriteClip(int x)
 
 }
 
-bool loadMedia()
+bool loadMedia(SDL_Renderer* gRenderer)
 {
     //Loading success flag
     bool success = true;
 
     //Load sprite sheet texture
-    if( !gSpriteSheetTexture.loadFromFile( "../resources/images/11.bmp" ) )
+    if( !gSpriteSheetTexture.loadFromFile( "../resources/images/11.bmp", gRenderer ) )
     {
         printf( "Failed to load sprite sheet texture!\n" );
         success = false;
@@ -245,6 +106,8 @@ bool loadMedia()
     return success;
 }
 
+
+//Frees media and shuts down SDL
 void close()
 {
     //Free loaded images
@@ -274,7 +137,7 @@ int main( int argc, char* args[] )
     else
     {
         //Load media
-        if( !loadMedia() )
+        if( !loadMedia(gRenderer) )
         {
             printf( "Failed to load media!\n" );
         }
@@ -320,11 +183,15 @@ int main( int argc, char* args[] )
                 }
 
                 if(isGoingRight) {
-                    CLIP_X += 1;
+                    if(CLIP_X < gSpriteSheetTexture.getWidth() - 640) {
+                        CLIP_X += 1;
+                    }
                     setSpriteClip(CLIP_X);
                 }
                 if(isGoingLeft) {
-                    CLIP_X -= 1;
+                    if(CLIP_X>0) {
+                        CLIP_X -= 1;
+                    }
                     setSpriteClip(CLIP_X);
                 }
                 //Clear screen
@@ -332,7 +199,7 @@ int main( int argc, char* args[] )
                 SDL_RenderClear( gRenderer );
 
                 //Render sprite
-                gSpriteSheetTexture.render( 0, 0, &gSpriteClip );
+                gSpriteSheetTexture.render( 0, 0, gRenderer, &gSpriteClip );
 
                 //Update screen
                 SDL_RenderPresent( gRenderer );
