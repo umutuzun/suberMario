57c06219cbb949cbb87b1b55c98f91aa59b9aaf6
Umut Uzun <umutu@evam.com>
2017-11-13 09:26:48 +0300

    create class windowmanager

--- suberMario.cpp
+++ suberMario.cpp
@@ -12,74 +12,93 @@ const int SCREEN_HEIGHT = 480;
 
 int CLIP_X = 0;
 
-//Starts up SDL and creates window
-bool init();
-
+class WindowManager {
 //The window renderer
-SDL_Renderer* gRenderer = NULL;
-
-//Loads individual image as texture
-SDL_Texture* loadTexture( std::string path );
-
+    SDL_Renderer *gRenderer = NULL;
 //The window we'll be rendering to
-SDL_Window* gWindow = NULL;
+    SDL_Window *gWindow = NULL;
 
-//Scene sprite
-SDL_Rect gSpriteClip;
-LTexture gSpriteSheetTexture;
+public:
 
-bool init()
-{
-    //Initialization flag
-    bool success = true;
-
-    //Initialize SDL
-    if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-    {
-        printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-        success = false;
+    SDL_Renderer *getGRenderer() const {
+        return gRenderer;
     }
-    else
-    {
-        //Set texture filtering to linear
-        if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-        {
-            printf( "Warning: Linear texture filtering not enabled!" );
-        }
 
-        //Create window
-        gWindow = SDL_CreateWindow( "Suber Mario", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-        if( gWindow == NULL )
+    bool init()
+    {
+        //Initialization flag
+        bool success = true;
+
+        //Initialize SDL
+        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
         {
-            printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
+            printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
             success = false;
         }
         else
         {
-            //Create renderer for window
-            gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-            if( gRenderer == NULL )
+            //Set texture filtering to linear
+            if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
             {
-                printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
+                printf( "Warning: Linear texture filtering not enabled!" );
+            }
+
+            //Create window
+            gWindow = SDL_CreateWindow( "Suber Mario", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+            if( gWindow == NULL )
+            {
+                printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
                 success = false;
             }
             else
             {
-                //Initialize renderer color
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-                //Initialize BMP loading
-                int imgFlags = IMG_INIT_PNG;
-                if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                //Create renderer for window
+                gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
+                if( gRenderer == NULL )
                 {
-                    printf( "SDL_image could not initialize! SDL_mage Error: %s\n", IMG_GetError() );
+                    printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
                     success = false;
+                }
+                else
+                {
+                    //Initialize renderer color
+                    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+
+                    //Initialize BMP loading
+                    int imgFlags = IMG_INIT_PNG;
+                    if( !( IMG_Init( imgFlags ) & imgFlags ) )
+                    {
+                        printf( "SDL_image could not initialize! SDL_mage Error: %s\n", IMG_GetError() );
+                        success = false;
+                    }
                 }
             }
         }
+        return success;
     }
-    return success;
-}
+
+    //Frees media and shuts down SDL
+    void close()
+    {
+        //Destroy window
+        SDL_DestroyRenderer( gRenderer );
+        SDL_DestroyWindow( gWindow );
+        gWindow = NULL;
+        gRenderer = NULL;
+
+        //Quit SDL subsystems
+        IMG_Quit();
+        SDL_Quit();
+    }
+};
+
+//Loads individual image as texture
+SDL_Texture* loadTexture( std::string path );
+
+
+//Scene sprite
+SDL_Rect gSpriteClip;
+LTexture gSpriteSheetTexture;
 
 void setSpriteClip(int x)
 {
@@ -107,37 +126,23 @@ bool loadMedia(SDL_Renderer* gRenderer)
 }
 
 
-//Frees media and shuts down SDL
-void close()
-{
-    //Free loaded images
-    gSpriteSheetTexture.free();
 
-    //Destroy window
-    SDL_DestroyRenderer( gRenderer );
-    SDL_DestroyWindow( gWindow );
-    gWindow = NULL;
-    gRenderer = NULL;
-
-    //Quit SDL subsystems
-    IMG_Quit();
-    SDL_Quit();
-}
 
 bool isGoingRight = false;
 bool isGoingLeft = false;
 
 int main( int argc, char* args[] )
 {
+    WindowManager windowManager;
     //Start up SDL and create window
-    if( !init() )
+    if( !windowManager.init() )
     {
         printf( "Failed to initialize!\n" );
     }
     else
     {
         //Load media
-        if( !loadMedia(gRenderer) )
+        if( !loadMedia(windowManager.getGRenderer()) )
         {
             printf( "Failed to load media!\n" );
         }
@@ -195,20 +200,23 @@ int main( int argc, char* args[] )
                     setSpriteClip(CLIP_X);
                 }
                 //Clear screen
-                SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-                SDL_RenderClear( gRenderer );
+                SDL_SetRenderDrawColor( windowManager.getGRenderer(), 0xFF, 0xFF, 0xFF, 0xFF );
+                SDL_RenderClear( windowManager.getGRenderer() );
 
                 //Render sprite
-                gSpriteSheetTexture.render( 0, 0, gRenderer, &gSpriteClip );
+                gSpriteSheetTexture.render( 0, 0, windowManager.getGRenderer(), &gSpriteClip );
 
                 //Update screen
-                SDL_RenderPresent( gRenderer );
+                SDL_RenderPresent( windowManager.getGRenderer() );
             }
         }
     }
 
+
+    //Free loaded images
+    gSpriteSheetTexture.free();
     //Free resources and close SDL
-    close();
+    windowManager.close();
 
     return 0;
 }
