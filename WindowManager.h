b4cc2c6ca9439dd609af79b621d7f2605e4d8d3f
Umut Uzun <umutu@evam.com>
2017-11-13 10:31:10 +0300

    move window manager to its own class & header

---
+++ WindowManager.h
@@ -0,0 +1,31 @@
+//
+// Created by Umut Uzun on 11/13/2017.
+//
+#ifndef SUBERMARIO_WINDOWMANAGER_H
+#define SUBERMARIO_WINDOWMANAGER_H
+
+
+#include <SDL2/SDL.h>
+#include <SDL2/SDL_image.h>
+#include <stdio.h>
+#include <string>
+#include "LTexture.h"
+#include "Constants.h"
+
+class WindowManager {
+//The window renderer
+    SDL_Renderer *gRenderer = NULL;
+//The window we'll be rendering to
+    SDL_Window *gWindow = NULL;
+
+public:
+
+    SDL_Renderer *getGRenderer() const {
+        return gRenderer;
+    }
+    bool init();
+    void close();
+};
+
+
+#endif //SUBERMARIO_WINDOWMANAGER_H
--- suberMario.cpp
+++ suberMario.cpp
@@ -5,92 +5,11 @@
 #include <string>
 #include "LTexture.h"
 #include "Constants.h"
+#include "WindowManager.h"
 
 int CLIP_X = 0;
-
-class WindowManager {
-//The window renderer
-    SDL_Renderer *gRenderer = NULL;
-//The window we'll be rendering to
-    SDL_Window *gWindow = NULL;
-
-public:
-
-    SDL_Renderer *getGRenderer() const {
-        return gRenderer;
-    }
-
-    bool init()
-    {
-        //Initialization flag
-        bool success = true;
-
-        //Initialize SDL
-        if( SDL_Init( SDL_INIT_VIDEO ) < 0 )
-        {
-            printf( "SDL could not initialize! SDL Error: %s\n", SDL_GetError() );
-            success = false;
-        }
-        else
-        {
-            //Set texture filtering to linear
-            if( !SDL_SetHint( SDL_HINT_RENDER_SCALE_QUALITY, "1" ) )
-            {
-                printf( "Warning: Linear texture filtering not enabled!" );
-            }
-
-            //Create window
-            gWindow = SDL_CreateWindow( "Suber Mario", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-            if( gWindow == NULL )
-            {
-                printf( "Window could not be created! SDL Error: %s\n", SDL_GetError() );
-                success = false;
-            }
-            else
-            {
-                //Create renderer for window
-                gRenderer = SDL_CreateRenderer( gWindow, -1, SDL_RENDERER_ACCELERATED );
-                if( gRenderer == NULL )
-                {
-                    printf( "Renderer could not be created! SDL Error: %s\n", SDL_GetError() );
-                    success = false;
-                }
-                else
-                {
-                    //Initialize renderer color
-                    SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
-
-                    //Initialize BMP loading
-                    int imgFlags = IMG_INIT_PNG;
-                    if( !( IMG_Init( imgFlags ) & imgFlags ) )
-                    {
-                        printf( "SDL_image could not initialize! SDL_mage Error: %s\n", IMG_GetError() );
-                        success = false;
-                    }
-                }
-            }
-        }
-        return success;
-    }
-
-    //Frees media and shuts down SDL
-    void close()
-    {
-        //Destroy window
-        SDL_DestroyRenderer( gRenderer );
-        SDL_DestroyWindow( gWindow );
-        gWindow = NULL;
-        gRenderer = NULL;
-
-        //Quit SDL subsystems
-        IMG_Quit();
-        SDL_Quit();
-    }
-};
-
-//Loads individual image as texture
-SDL_Texture* loadTexture( std::string path );
-
+bool isGoingRight = false;
+bool isGoingLeft = false;
 
 //Scene sprite
 SDL_Rect gSpriteClip;
@@ -101,8 +20,8 @@ void setSpriteClip(int x)
     //Set sprite
     gSpriteClip.x =   x;
     gSpriteClip.y =   0;
-    gSpriteClip.w = 640;
-    gSpriteClip.h = 480;
+    gSpriteClip.w = SCREEN_WIDTH;
+    gSpriteClip.h = SCREEN_HEIGHT;
 
 }
 
@@ -121,11 +40,6 @@ bool loadMedia(SDL_Renderer* gRenderer)
     return success;
 }
 
-
-
-
-bool isGoingRight = false;
-bool isGoingLeft = false;
 
 int main( int argc, char* args[] )
 {
